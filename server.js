e6a76d059babeebdb039caafdfe92097f3145ec2
Jesús <jrodriguez@axonico.net>
2022-10-17 14:13:16 -0300

    Plantillas

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const routerProductos = require('./routers/productos')
-
+const handlebars= require("express-handlebars")
+const path = require("path")
 const app = express()
 
-app.listen(8080, ()=>console.log('Servidor Activo'))
+const folderViews = path.join(__dirname, "views")
 
+app.listen(8080, ()=>console.log('Servidor Activo'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-
+app.engine("handlebars", handlebars.engine())
+app.set("views",folderViews)
 
 app.use('/api', routerProductos)
+
 
 
 
