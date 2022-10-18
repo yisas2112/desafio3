805cd897305ee370748c09ae4b0d0e90b65c89c1
Jesús <jrodriguez@axonico.net>
2022-10-18 14:42:13 -0300

    Handlebars v2

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const routerProductos = require('./routers/productos')
 const handlebars= require("express-handlebars")
 const path = require("path")
 const app = express()
-const folderViews = path.join(__dirname, "views")
 
 app.listen(8080, ()=>console.log('Servidor Activo'))
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.engine("handlebars", handlebars.engine())
-app.set("views",folderViews)
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "handlebars");
 
 app.use('/api', routerProductos)
